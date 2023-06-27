e9ddea8fd8a2491edcb02d1d563242fba7ef810c
Cuc6mb6r <romanmalishev737@gmail.com>
2023-06-27 17:49:37 +0300

    add player

--- main.py
+++ main.py
@@ -1,2 +1,34 @@
-print("Hello World!")
-print("Still here")
+import pygame
+w = pygame.display.set_mode((720, 470))
+class Sprite():
+    image = pygame.image.load('assets/Lizard_front1.png').convert_alpha()
+
+
+    speed = 5
+    x = 100
+    y = 100
+    def move(self, side):
+        if side == "left":
+            self.x -= self.speed
+        if side == "right":
+            self.x += self.speed
+
+time = pygame.time.Clock()
+Player = Sprite()
+
+game = True
+while game:
+    for ev in pygame.event.get():
+        if ev.type == pygame.QUIT:
+            game = False
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_d]:
+        Player.move('right')
+    if keys[pygame.K_a]:
+        Player.move('left')
+    w.fill((0,0,0))
+    w.blit(Player.image, (Player.x,Player.y))
+
+    time.tick(30)
+    pygame.display.update()
+pygame.quit()
